212f6242229c62fe778683ae2f1bb4e5c6ce8404
Ty <tycloud97@gmail.com>
2024-11-13 22:24:05 +0700

    ty

--- simple-web-server/server.js
+++ simple-web-server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! This server is running on Node.js with Express V2.');
+  res.send('Hello World! This server is running on Node.js with Express V4.');
 });
 
 app.get('/info', (req, res) => {
